eb24c33260e598532147163f88d7973ea4fde758
Loopr0 <147174159+Loopr0@users.noreply.github.com>
2023-11-13 21:19:46 +0100

    Update equation_onde_1D_bord.py

--- equation_onde_1D_bord.py
+++ equation_onde_1D_bord.py
@@ -3,21 +3,22 @@ import matplotlib.pyplot as plt
 import matplotlib.animation as animation
 
 """
-programme pour simuler la propagation d'une onde en 1D sur une longueur L.
+programme pour simuler la propagation d'une onde en 1D sur une longueur L avec condition au limite tels que soit :
+- reflexion totale
+- reflexion totale inversé
+- sans bord
 
-pour pouvoir avoir une solution la plus cohérente avec la physique, la condition CFL est utilisé
-pour choisir nos paramètres. Cette condition dis que udt/dx = CFL
+le cas d'une atténuation au bord sera traité dans un autre programme car pas la même équation d'onde.
+
+pour pouvoir avoir une solution la plus cohérente avec la physique, la condition CFL est utilisé pour choisir nos paramètres.
+Cette condition dis que udt/dx = CFL
 """
 
-
-
-##################################
-
-
+#définition des paramètres du problème
 c = 100 # Vitesse de l'onde
 L = 5  # Longueur de la corde
 T = 0.1  # Temps total
-K = 0.4 #coef d'atténuation au bord
+
 dx = 0.01
 dt = 0.0001
 Nx = int(L/dx) # Nombre de points spatiaux
@@ -27,39 +28,41 @@ x = np.linspace(0,L,Nx)
 x_init = L/2 #onde placée au centre de la longueur L
 
 
-####################################
+
 
 
 #définition des conditions initial comme une gaussienne
 def init_cond(x):
-    return np.exp(-(x-x_init)**2/dx)
+    return np.exp(-(x-x_init)**2/0.1)
 
 
-#condition au bord : 0 = dirichelet, 1 = Neumann, 2 = sans reflexion
-Bord_Lx = 2
-Bord_0x = 2
+#condition au bord : 0 = dirichelet, 1 = Neumann, 2 = sans bord
+Bord_Lx = 1
+Bord_0x = 1
 
+#mise en place des matrices utiles, avec U placée au condition initiale x_init
 U = init_cond(x)
 Uold = U.copy()
 Unew = np.zeros_like(x)
 
-###################################
 
 
-
-fig = plt.figure()
-ax = plt.axes(xlim= (0,L) , ylim= (-0.6,1))
+#initialisation de la figure et de la courbe du plot
+fig, ax = plt.subplots()
+ax.set_xlim(0, L)
+ax.set_ylim(-0.6, 1)
 ax.grid()
 line, = ax.plot(x, U)
 
-
-##################################
 
 def update(frame):
     global U, Uold, Unew
 
     Unew[1:-1] = (c*dt/dx)**2*(U[2:] - 2*U[1:-1] + U[:-2]) + 2*U[1:-1] - Uold[1:-1]
     #en prenant cette intervalle ]0,L[ on ne prend pas en compte les limites au bord qui sont pris en compte dans les if
+
+    Uold = U.copy()
+    U = Unew.copy()
 
 
 #condition au limite gauche x = 0
@@ -70,8 +73,7 @@ def update(frame):
         U[0] = U[1] #dans cette condition, l'amplitude en 0 dois valoir l'amplitude qui arrive sur la paroi
 
     elif Bord_0x == 2:
-        Unew[0] = U[1] + K*( Unew[1] - U[0]) #pb après un certain temps gros bordel venant du bord (arrive plus vite quand K augmente)
-        #+ pas bonne valeur d'atténuation
+        Unew[0] = U[1] + ( Unew[0] - U[1]) #condition de continuité, propagation sans bord
 
 
 #condition au limite droite x = L
@@ -82,12 +84,9 @@ def update(frame):
         U[-1] = U[-2]
 
     elif Bord_Lx == 2:
-        Unew[-1] = U[-2] + K*( Unew[-1] - U[-2]) #de ce côté aucun pb pas bonne valeur d'atténuation
+        Unew[-1] = U[-2] + ( Unew[-1] - U[-2]) 
     
-    
-    Uold = U.copy()
-    U = Unew.copy()
-#pb réglé de bordel en mettant Uold et U ici mais mtn satisfait pas les bonne condition limite à droite (quand 2 fait 1 avec 0)
+
     line.set_ydata(U)
 
     return line,
